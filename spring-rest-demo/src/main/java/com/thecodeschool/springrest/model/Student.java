20c313102863aea409b717310fdadb5e0a182185
thecodeschool-nk <nirajkumar@Nirajs-MacBook-Air.local>
2022-08-05 12:18:33 +0530

    added spring rest demo

---
+++ spring-rest-demo/src/main/java/com/thecodeschool/springrest/model/Student.java
@@ -0,0 +1,61 @@
+package com.thecodeschool.springrest.model;
+
+import java.util.Date;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private String address;
+    private Date dateOfBirth;
+
+    public Student() {}
+    public Student(long id, String name, String address, Date dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
